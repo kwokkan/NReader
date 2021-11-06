63d69abf1bbfa3fedf13729f773a0165a8e75874
kwokkan <kwokkan@users.noreply.github.com>
2021-11-06 18:41:30 +0000

    #5: Add additional classes required to get pages out of an article.

---
+++ src/NReader.Abstractions/Services/ArticleLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NReader.Abstractions
+{
+    public abstract class ArticleLoader
+    {
+        private static readonly IReadOnlyCollection<Article> Empty = Array.Empty<Article>();
+
+        public virtual Task<IReadOnlyCollection<Article>> GetArticlesAsync()
+        {
+            return Task.FromResult(Empty);
+        }
+    }
+}
