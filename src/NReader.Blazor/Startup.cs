b579cd6d67c158ede30c40d1b1ba8f266ae96241
kwokkan <kwokkan@users.noreply.github.com>
2021-11-06 20:00:06 +0000

    #5: Add test source to UI.

--- src/NReader.Blazor/Startup.cs
+++ src/NReader.Blazor/Startup.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +26,8 @@ namespace NReader.Blazor
             services.AddSingleton<WeatherForecastService>();
 
             services.AddNReaderCore();
+
+            services.AddNReaderStaticSources();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
