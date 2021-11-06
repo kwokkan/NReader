6026e94f7eef858a955a78c78d165c0b452bf7f9
kwokkan <kwokkan@users.noreply.github.com>
2021-11-06 16:38:10 +0000

    #5: Add test source.

---
+++ extensions/NReader.Extensions.Test/TestSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using NReader.Abstractions;
+
+namespace NReader.Extensions.Test
+{
+    public class TestSource : Source
+    {
+        public override string Title => "Test Source";
+
+        public override Uri Url => new Uri("https://localhost/");
+    }
+}
