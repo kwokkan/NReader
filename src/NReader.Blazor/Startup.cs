d03a4b94209b78132223a5374135db5f50f67a80
kwokkan <kwokkan@users.noreply.github.com>
2021-11-06 16:20:50 +0000

    #3: Remove unused pages.

--- src/NReader.Blazor/Startup.cs
+++ src/NReader.Blazor/Startup.cs
@@ -1,17 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-using NReader.Blazor.Data;
 
 namespace NReader.Blazor
 {
@@ -30,7 +21,6 @@ namespace NReader.Blazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
 
             services.AddNReaderCore();
         }
