9780b0fc2978c82e111f7efa3ed9016a4eed8c90
kwokkan <kwokkan@users.noreply.github.com>
2022-02-19 23:21:41 +0000

    #15: Allow read status to be saved.

--- src/NReader.Storage.Abstractions/IStorageProvider.cs
+++ src/NReader.Storage.Abstractions/IStorageProvider.cs
@@ -2,5 +2,13 @@
 
 public interface IStorageProvider
 {
+    /// <summary>
+    /// Initialise the storage.
+    /// 
+    /// Must be called before any other methods are used.
+    /// </summary>
+    /// <returns></returns>
     Task InitialiseAsync();
+
+    Task ReadArticlesAsync(string userId, IEnumerable<string> articleIds);
 }
